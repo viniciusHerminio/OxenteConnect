7549823b14afe34300e0e5c6f13a642e3f9510c6
Vinicius Herminio <viniciusherminio1@outlook.com>
2023-09-27 17:17:40 -0300

    cards adicionados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 import logo from './images/logoOxente.png';
 import Banner from './images/bannerSite.png';
+import nuvem from './images/nuvem.png';
+import mobilidade from './images/mobilidade.png';
+import financeiro from './images/financeiro.png';
+import gestao from './images/gestão.png';
 import './App.css';
+
+const benefitsData = [
+  {
+    icon: nuvem,
+    title: 'Integração com o Provedor de Internet',
+    description: 'Integração com nosso provedor de internet para maior segurança, velocidade e fácil manutenção em todo o sistema.'
+  },
+  {
+    icon: mobilidade,
+    title: 'Mobilidade e Flexibilidade',
+    description: 'Permite a comunicação de qualquer lugar, proporcionando maior mobilidade e flexibilidade para os usuários, facilitando o trabalho remoto e deslocamentos.'
+  },
+  {
+    icon: financeiro,
+    title: 'Economia de Custos',
+    description: 'Reduz os custos operacionais e de manutenção, pois utiliza a infraestrutura de rede existente e elimina a necessidade de hardware físico extenso.'
+  },
+  {
+    icon: gestao,
+    title: 'Gestão e Monitoramento Avançados',
+    description: 'Oferece recursos avançados de gestão e monitoramento em tempo real, possibilitando um controle mais eficaz das chamadas, o que contribui para melhorar a eficiência da comunicação empresarial.'
+  }
+];
 
 function App() {
   const [showForm, setShowForm] = useState(false);
@@ -14,17 +41,9 @@ function App() {
   const handleWhatsappChange = (event) => {
     const inputValue = event.target.value;
 
-    // Remove todos os caracteres não numéricos
     const numericValue = inputValue.replace(/\D/g, '');
-
-    // Limita a 11 caracteres (máximo de um número de celular)
     const limitedValue = numericValue.slice(0, 11);
-
-    // Formata o número no formato de celular (XX) XXXXX-XXXX
-    const formattedValue = limitedValue.replace(
-      /(\d{2})(\d{5})(\d{4})/,
-      '($1) $2-$3'
-    );
+    const formattedValue = limitedValue.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
 
     setWhatsapp(formattedValue);
   };
@@ -49,13 +68,12 @@ function App() {
           <form className="Form">
             <div className="FormField">
               <label htmlFor="nome">Nome completo:</label>
-              <input type="text" id="nome" name="nome" required placeholder="Seu Nome Completo" />
+              <input type="text" id="nome" name="nome" required />
             </div>
 
             <div className="FormField">
               <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" required placeholder="email@email.email" />
-              
+              <input type="email" id="email" name="email" required />
             </div>
 
             <div className="FormField">
@@ -76,6 +94,22 @@ function App() {
           </form>
         </div>
       )}
+
+      {/* Cards de benefícios */}
+      <h1 className='BenefitsTitle'>Benefícios</h1>
+      <div className="BenefitsContainer">
+        {benefitsData.map((benefit, index) => (
+          <div className="BenefitCard" key={index}>
+            <div className="CardHeader">
+              <img src={benefit.icon} alt="Ícone de nuvem" className="CardIcon" />
+              <h3 className="CardTitle">{benefit.title}</h3>
+            </div>
+            <div className="CardContent">
+              <p className={`descricao${index}`}>{benefit.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
