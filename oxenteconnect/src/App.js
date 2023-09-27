e29489572ce48a0fde52ee5ca4b72a946d7cdfb2
Vinicius Herminio <viniciusherminio1@outlook.com>
2023-09-27 09:18:00 -0300

    head created

---
+++ oxenteconnect/src/App.js
@@ -0,0 +1,12 @@
+import logo from './images/logoOxente.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Header">
+      <img src={logo} className="Logo" alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
