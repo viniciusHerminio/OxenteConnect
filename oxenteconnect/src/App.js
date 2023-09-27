52fad3084740efe75e5be098db611e8af9da4979
Vinicius Herminio <viniciusherminio1@outlook.com>
2023-09-27 11:27:48 -0300

    Banner adicionado a página

--- oxenteconnect/src/App.js
+++ oxenteconnect/src/App.js
@@ -1,10 +1,18 @@
 import logo from './images/logoOxente.png';
+import Banner from './images/bannerSite.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="Header">
-      <img src={logo} className="Logo" alt="Logo" />
+    <div>
+      <div className="Header">
+        <img src={logo} className="Logo" alt="Logo" />
+          <div className='opHeader'>
+           <h5 className='contato'>Contato</h5>
+           <h5 className='plano'>Planos</h5>
+         </div>
+      </div>
+      <img className='Banner' src={Banner} alt='Banner'/>
     </div>
   );
 }
