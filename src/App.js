9752c818a2e55c697c5b6bb3d7a222f0b0a16f88
Vinicius Herminio <viniciusherminio1@outlook.com>
2023-09-28 13:05:16 -0300

    tudo funcionando faltando apenas ajustar cards de planos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './images/logoOxente.png';
 import Banner from './images/bannerSite.png';
 import nuvem from './images/nuvem.png';
@@ -30,22 +30,71 @@ const benefitsData = [
   }
 ];
 
+const plansData = [
+  {
+    name: 'Plano Básico',
+    price: '$29.99/mês',
+    description: 'Ideal para pequenos negócios que estão começando.',
+    beneficios: ['beneficio1','beneficio2','beneficio3', 'beneficio4', 'beneficio5']
+  },
+  {
+    name: 'Plano Padrão',
+    price: '$49.99/mês',
+    description: 'Ótima opção para negócios em crescimento com necessidades mais avançadas.',
+    beneficios: ['beneficio1','beneficio2','beneficio3', 'beneficio4', 'beneficio5']
+  },
+  {
+    name: 'Plano Avançado',
+    price: '$79.99/mês',
+    description: 'Para negócios estabelecidos que exigem funcionalidades avançadas e suporte dedicado.',
+    beneficios: ['beneficio1','beneficio2','beneficio3', 'beneficio4', 'beneficio5']
+  }
+];
+
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [whatsapp, setWhatsapp] = useState('');
 
   const handleContatoClick = () => {
     setShowForm(!showForm);
+
+    const video = document.querySelector('.VideoSection iframe');
+    if (video) {
+      video.classList.toggle('blur-video', showForm);
+    }
   };
 
   const handleWhatsappChange = (event) => {
     const inputValue = event.target.value;
-
     const numericValue = inputValue.replace(/\D/g, '');
     const limitedValue = numericValue.slice(0, 11);
     const formattedValue = limitedValue.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
-
     setWhatsapp(formattedValue);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const planCards = document.querySelectorAll('.PlanCard');
+      planCards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+          card.classList.add('in-view');
+        } else {
+          card.classList.remove('in-view');
+        }
+      });
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();  // Chame a função inicialmente para os cards visíveis ao carregar a página
+  
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleToggleForm = () => {
+    setShowForm(!showForm);
   };
 
   return (
@@ -63,7 +112,10 @@ function App() {
       <img className={`Banner ${showForm ? 'blur-background' : ''}`} src={Banner} alt='Banner' />
 
       {showForm && (
-        <div className="FormContainer">
+        <div className={`FormContainer ${showForm ? 'show' : ''}`}>
+          <div className="CloseIcon" onClick={handleToggleForm}>
+            &#x2715;
+          </div>
           <h2 className="FormTitle">Preencha seus dados de contato:</h2>
           <form className="Form">
             <div className="FormField">
@@ -90,25 +142,72 @@ function App() {
               />
             </div>
 
+            <div className="FormField">
+              <label htmlFor="menssagem">Menssagem:</label>
+              <textarea className='text' placeholder='Deixe sua mensagem que entraremos em contato o mais breve possível!' rows="4" cols="50" name="comment" form="usrform"/>
+            </div>
+
             <button type="submit" className="SubmitButton">Enviar</button>
           </form>
         </div>
       )}
 
-      <h1 className={`BenefitsTitle ${showForm ? 'blur-text' : ''}`}>Benefícios</h1>
-      <div className={`BenefitsContainer ${showForm ? 'blur-background' : ''}`}>
-        {benefitsData.map((benefit, index) => (
-          <div className="BenefitCard" key={index}>
-            <div className="CardHeader">
-              <img src={benefit.icon} alt="Ícone de nuvem" className="CardIcon" />
-              <h3 className="CardTitle">{benefit.title}</h3>
-            </div>
-            <div className="CardContent">
-              <p className={`descricao${index}`}>{benefit.description}</p>
-            </div>
-          </div>
-        ))}
+      <div className={`VideoSection${showForm ? 'blur-text' : ''}`}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/qrHNeZ1-obg"
+          title="PABX Virtual"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <div className="VideoTitleContainer">
+          <h1 className={`VideoTitle${showForm ? 'blur-text' : ''}`}>Assista a esse vídeo</h1>
+          <p className={`Videodescricao${showForm ? 'blur-text' : ''}`}>E entenda o porquê o nosso PABX virtual irá <br/> elevar a comunicação da sua empresa a outro nível.</p>
+          <h1 className={`VideoTitle${showForm ? 'blur-text' : ''}`}>Dúvidas?</h1>
+          <p className={`Videodescricao${showForm ? 'blur-text' : ''}`}>Se restar mais alguma dúvida, entre em contato <br/> com nosso time comercial. Estaremos ansiosos em lhe atender.</p>
+        </div>
       </div>
+
+      <div className={`beneficioscompleto${showForm ? 'blur-text' : ''}`}>
+        <h1 className={`BenefitsTitle ${showForm ? 'blur-text' : ''}`}>Benefícios</h1>
+        <div className={`BenefitsContainer ${showForm ? 'blur-background' : ''}`}>
+          {benefitsData.map((benefit, index) => (
+            <div className="BenefitCard" key={index}>
+              <div className="CardHeader">
+                <img src={benefit.icon} alt="Ícone de nuvem" className="CardIcon" />
+                <h3 className="CardTitle">{benefit.title}</h3>
+              </div>
+              <div className="CardContent">
+                <p className={`descricao${index}`}>{benefit.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+      <h1 className={`BenefitsTitle ${showForm ? 'blur-text' : ''}`}>Confira Nossos Planos</h1>
+      <div className={`PlansContainer${showForm ? 'blur-text' : ''}`}>
+      {plansData.map((plan, index) => (
+  <div className="PlanCard" key={index}>
+    <div className="CardHeader">
+      <h3 className="CardTitle">{plan.name}</h3>
+      <p className="PlanPrice">{plan.price}</p>
+    </div>
+    <div className="CardContent">
+      <p className={`planodescricao${index}`}>{plan.description}</p>
+      <h4>Benefícios:</h4>
+      <ul>
+        {plan.beneficios.map((beneficio, idx) => (
+          <li key={idx}>{beneficio}</li>
+        ))}
+      </ul>
+    </div>
+  </div>
+))}
+      </div>
+    </div>
     </div>
   );
 }
